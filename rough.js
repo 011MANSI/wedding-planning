d505b3d893a12641f5fd7449559d7e31823b7800
011MANSI <101451468+011MANSI@users.noreply.github.com>
2023-09-10 16:02:40 +0530

    Add files via upload

---
+++ rough.js
@@ -0,0 +1,12 @@
+<script language="JavaScript" type="text/javascript" >
+function Greet(who) {
+alert("Greetings, " + who);
+}
+</script>
+$(".click-me a").click(function(){
+    $("#ad_position_box").addClass("active");
+  });
+  $(".skip").click(function(){
+    $("#ad_position_box").removeClass("active");
+  });
+  
